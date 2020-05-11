682823e2134e4571fcf19abee052906bb6bb28b9
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-05-11 21:36:42 +0900

    add comment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 function App() {
   return (
     // ↓のhtmlのタグのような部分がJSX（JavaScriptXml）
+    // Reactを用いてhtmlを出力するための言語
     // ここの部分はJavaScriptに変換されるが、変換後の素のJSを記述するより
     // JSXを用いたほうがより出力イメージに近い形で記述できるのでJSXが用いられる。
     <h1>Hello, React!!</h1>
