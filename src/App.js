79088f92c86a4d9272843774da4333965dd080ff
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-05-17 17:46:18 +0900

    create class components and functional components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
-function App() {
-  return (
-    // return内は一つのタグで囲わないといけない
-    // そうすると不必要なhtmlタグが出力されてしまう
-    // それを避けるためにreturn内を<React.Fragment></React.Fragment>で囲うと
-    // 不要なhtmlタグが出力されなくなる
-    // htmlのfor、classはJSX内ではそれぞれhtmlFor、classNameとする
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input id="bar" type="text" onClick={() => {console.log("Hello!")}} />
-    </React.Fragment>
+// function App() {
+//   return (
+//     // return内は一つのタグで囲わないといけない
+//     // そうすると不必要なhtmlタグが出力されてしまう
+//     // それを避けるためにreturn内を<React.Fragment></React.Fragment>で囲うと
+//     // 不要なhtmlタグが出力されなくなる
+//     // htmlのfor、classはJSX内ではそれぞれhtmlFor、classNameとする
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input id="bar" type="text" onClick={() => {console.log("Hello!")}} />
+//     </React.Fragment>
     
-  );
+//   );
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow</div>
 }
 
 export default App;
