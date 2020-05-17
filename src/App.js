d42c2b0a30f670bd0ae004b0ac0bd01709d16096
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-05-17 18:13:29 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
 
-// function App() {
-//   return (
-//     // return内は一つのタグで囲わないといけない
-//     // そうすると不必要なhtmlタグが出力されてしまう
-//     // それを避けるためにreturn内を<React.Fragment></React.Fragment>で囲うと
-//     // 不要なhtmlタグが出力されなくなる
-//     // htmlのfor、classはJSX内ではそれぞれhtmlFor、classNameとする
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input id="bar" type="text" onClick={() => {console.log("Hello!")}} />
-//     </React.Fragment>
-    
-//   );
-// }
+// props
+// 親コンポーネントから子コンポーネントへデータを渡す際にpropsを使用する
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10},
+    { name: "Hanako", age: 5},
+    { name: "NoName" }
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+      {/* <User name={"Taro"} age={10} /> User コンポーネントに対してname属性「Taro」を渡している */}
+      {/* <User name={"Hanako"} age={5} /> */}
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow</div>
+const User = (props) => {
+  return <div>Hi!, I am {props.name}, and {props.age} years old! </div>
+}
+
+// デフォルトprops
+// 個別のデータを渡さない場合のデフォルト値を定めておくことができる
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
