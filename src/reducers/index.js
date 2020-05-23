6c1e0e010f4937ff01bbd26f33152dcedcb1f8e2
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-05-23 11:34:35 +0900

    implement reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+// reducer
+// アクショんが発生した時に、そのアクションに組み込まれているtypeに応じて状態をどう変化させるかを定義するもの
+// reducerディレクトリ内のindex.jsでは全reducerを総括するためのファイル
+// 全reducerを1つのreducerに結合させる
+
+import { combineReducers } from 'redux'
+import count from './count'
+
+export default combineReducers({ count })
